5a3969a5a57fd6c4ac78eae166d537db21d71b50
Santiago <santigs211@gmail.com>
2025-11-19 23:13:03 -0300

    feat: Update AnimeLi component to include episode icon in the episode display

--- components/anime/table/AnimeLi.tsx
+++ components/anime/table/AnimeLi.tsx
@@ -1,20 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { Episode } from '../../../types/Episode';
+import Clock from "@/components/general-components/icons/Clock";
 
 interface Props {
     coverImage: string;
     title: string;
     id: number;
-    episode:number;
+    episode: number;
 }
 export default function AnimeLi({ coverImage, title, id, episode }: Props) {
     return (
         <li className="text-center relative flex flex-col items-center gap-1  m-2 bg-[#0d1b2a] text-pretty  h-[170px] 2xl:h-[230] overflow-hidden rounded-lg ">
             <Link href={`/anime/${id}`} className="flex flex-col w-full h-full justify-end  items-start ">
                 <div className="absolute inset-0 bg-gradient-to-b from-transparent  via-black/30 to-black/90 z-20" />
-                <span className="text-xs 2xl:text-sm  z-20  flex  text-left ml-1 rounded-lg px-2 py-0.5  bg-[#12161F] ">EP {episode}</span>
+                <span className="text-xs 2xl:text-sm  z-20  flex  text-left ml-1 rounded-lg px-2 py-0.5 items-center bg-[#12161F] gap-1 ">
+                    <Clock width={15} height={15}></Clock>EP {episode}
+                </span>
                 <p className="text-xs 2xl:text-sm font-semibold z-20  text-left pl-2 w-full mb-1  ">{title}</p>
                 <Image src={coverImage} alt={title} width={2560} height={1440} className="absolute top-0 left-0  z-10  rounded-lg object-fill" />
             </Link>
