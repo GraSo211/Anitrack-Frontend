572801e651abba622006f112f6f990aef5a8b3c5
GraSo <santigs211@gmail.com>
2026-02-08 23:22:44 -0300

    fix: add revalidation settings for caching in getAnimeById action

--- actions/getAnimeById.ts
+++ actions/getAnimeById.ts
@@ -2,9 +2,12 @@
 export const getAnimeById = async (id: number) => {
     const response = await fetch(`${process.env.BACKEND_URL}/api/v1/anime/${id}`, {
         method: "GET",
+        next: {
+            revalidate: 86400, // 1 día
+        },
     });
 
     const data = await response.json();
 
-    return data; 
+    return data;
 };
