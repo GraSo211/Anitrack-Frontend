78ea6167fbbda1db37f32d6f644921d78f0c921b
GraSo <santigs211@gmail.com>
2026-02-10 20:42:31 -0300

    fix null on categories

--- components/general-components/AnimeCategories.tsx
+++ components/general-components/AnimeCategories.tsx
@@ -1,29 +1,34 @@
-import React from 'react'
-import AnimeList from '../anime/horizontal-list/AnimeList'
-import { getSeasonTrendAnimes } from '@/actions/categories/getSeasonTrendAnimes';
-import { getUpcomingAnimeReleases } from '@/actions/categories/getUpcomingAnimeReleases';
-import { getMostValoratedAnimes } from '@/actions/categories/getMostValoratedAnimes';
-import { getAnimesByGenre } from '@/actions/categories/getAnimesByGenre';
-import { AnimeCard } from '@/types/AnimeCard';
+import React from "react";
+import AnimeList from "../anime/horizontal-list/AnimeList";
+import { getSeasonTrendAnimes } from "@/actions/categories/getSeasonTrendAnimes";
+import { getUpcomingAnimeReleases } from "@/actions/categories/getUpcomingAnimeReleases";
+import { getMostValoratedAnimes } from "@/actions/categories/getMostValoratedAnimes";
+import { getAnimesByGenre } from "@/actions/categories/getAnimesByGenre";
+import { AnimeCard } from "@/types/AnimeCard";
 
-export default async  function AnimeCategories() {
-    const seasonTrendAnimes: AnimeCard[] = await getSeasonTrendAnimes();
-    const upcomingAnimes: AnimeCard[] = await getUpcomingAnimeReleases();
-    const mostValoratedAnimes: AnimeCard[] = await getMostValoratedAnimes();
-    const actionAnimes: AnimeCard[] = await getAnimesByGenre("Action");
-    const romanceAnimes: AnimeCard[] = await getAnimesByGenre("Romance");
-    const comedyAnimes: AnimeCard[] = await getAnimesByGenre("Comedy");
-    const sliceOfLifeAnimes: AnimeCard[] = await getAnimesByGenre("Slice of Life");
-  return (
-    <div>
-         <AnimeList category={seasonTrendAnimes} title="Lo más popular de la Temporada" />
-         <AnimeList category={upcomingAnimes} title="Estrenos más Esperados" />
-         <AnimeList category={mostValoratedAnimes} title="Mejor Valorados de la Historia" />
-         <AnimeList category={actionAnimes} title="Acción" />
-         <AnimeList category={romanceAnimes} title="Romance" />
-         <AnimeList category={comedyAnimes} title="Comedia" />
-         <AnimeList category={sliceOfLifeAnimes} title="Slice of Life" />
-         
-    </div>
-  )
+export default async function AnimeCategories() {
+    const seasonTrendAnimes: AnimeCard[] | null = await getSeasonTrendAnimes();
+    const upcomingAnimes: AnimeCard[] | null = await getUpcomingAnimeReleases();
+    const mostValoratedAnimes: AnimeCard[] | null = await getMostValoratedAnimes();
+    const actionAnimes: AnimeCard[] | null = await getAnimesByGenre("Action");
+    const romanceAnimes: AnimeCard[] | null = await getAnimesByGenre("Romance");
+    const comedyAnimes: AnimeCard[] | null = await getAnimesByGenre("Comedy");
+    const sliceOfLifeAnimes: AnimeCard[] | null = await getAnimesByGenre("Slice of Life");
+    return (
+        <div>
+            {seasonTrendAnimes && <AnimeList category={seasonTrendAnimes} title="Lo más popular de la Temporada" />}
+
+            {upcomingAnimes && <AnimeList category={upcomingAnimes} title="Estrenos más Esperados" />}
+
+            {mostValoratedAnimes && <AnimeList category={mostValoratedAnimes} title="Mejor Valorados de la Historia" />}
+
+            {actionAnimes && <AnimeList category={actionAnimes} title="Acción" />}
+
+            {romanceAnimes && <AnimeList category={romanceAnimes} title="Romance" />}
+
+            {comedyAnimes && <AnimeList category={comedyAnimes} title="Comedia" />}
+
+            {sliceOfLifeAnimes && <AnimeList category={sliceOfLifeAnimes} title="Slice of Life" />}
+        </div>
+    );
 }
